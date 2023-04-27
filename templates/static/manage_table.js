da99b41615955d3687f77eb78a6c1046c6fca977
dusanlecic <dusan.lecic5@gmail.com>
2023-04-27 18:22:02 +0200

    simplifed js

--- templates/static/manage_table.js
+++ templates/static/manage_table.js
@@ -1,130 +1,75 @@
-/*
 function createGrid() {
-  // Get grid width and height from inputs
-  var gridWidth = document.getElementById("grid-width").value;
-  var gridHeight = document.getElementById("grid-height").value;
-  var isMouseDown = false;
+  // Initialize variables for cell coloring
+  let maxBlueCells = 2;
   let blueCells = 0;
-  const maxBlueCells = 2;
 
-  // Get container element
-  var container = document.getElementById("container");
+  // Get grid dimensions from inputs
+  const gridWidth = Number(document.getElementById("grid-width").value);
+  const gridHeight = Number(document.getElementById("grid-height").value);
 
-  // Clear previous grid, if any
+  // Get container element and clear previous grid
+  const container = document.getElementById("container");
   container.innerHTML = "";
-
-  // Create grid cells
-  for (var i = 0; i < gridWidth * gridHeight; i++) {
-    var cell = document.createElement("div");
-    cell.classList.add("cell");
-
-    cell.addEventListener('mousedown', () => {
-        isMouseDown = true;
-        if (!cell.classList.contains('red')) {
-          if (cell.classList.contains('blue')) {
-            cell.classList.remove('blue');
-            blueCells--;
-          } else if (blueCells < maxBlueCells) {
-            cell.classList.add('blue');
-            blueCells++;
-          }
-        }
-      });
-
-      cell.addEventListener('mouseenter', () => {
-        if (isMouseDown) {
-          if (!cell.classList.contains('blue') && !cell.classList.contains('red')) {
-            cell.classList.add('red');
-          } else if (cell.classList.contains('red')) {
-            cell.classList.remove('red');
-          }
-        }
-      });
-
-      cell.addEventListener('mouseup', () => {
-        isMouseDown = false;
-      });
-
-      cell.addEventListener('contextmenu', (event) => {
-        event.preventDefault();
-        if (!cell.classList.contains('blue')) {
-          if (cell.classList.contains('red')) {
-            cell.classList.remove('red');
-          } else {
-            cell.classList.add('red');
-          }
-        }
-      });
-      container.appendChild(cell);
-  }
 
   // Set container width and height based on grid size
-  container.style.width = gridWidth * 50 + "px";
-  container.style.height = gridHeight * 50 + "px";
-}
-*/
-function createGrid() {
-  // Get grid width and height from inputs
-  var gridWidth = document.getElementById("grid-width").value;
-  var gridHeight = document.getElementById("grid-height").value;
+  container.style.width = `${gridWidth * 25}px`;
+  container.style.height = `${gridHeight * 25}px`;
 
-  // Get container element
-  var container = document.getElementById("container");
 
-  // Clear previous grid, if any
-  container.innerHTML = "";
-
-  // Create grid cells
-  for (var i = 0; i < gridWidth * gridHeight; i++) {
+  // Create grid cells and attach event listeners
+  for (let i = 0; i < gridWidth * gridHeight; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.setAttribute("data-row", Math.floor(i / gridWidth));
-    cell.setAttribute("data-col", i % gridWidth);
+    cell.dataset.row = Math.floor(i / gridWidth);
+    cell.dataset.col = i % gridWidth;
 
+    cell.addEventListener("mousedown", handleMouseDown);
+    cell.addEventListener("mouseenter", handleMouseEnter);
+    cell.addEventListener("mouseup", handleMouseUp);
+    cell.addEventListener("contextmenu", handleContextMenu);
 
-    cell.addEventListener('mousedown', () => {
-      isMouseDown = true;
-      if (!cell.classList.contains('red')) {
-        if (cell.classList.contains('blue')) {
-          cell.classList.remove('blue');
-          blueCells--;
-        } else if (blueCells < maxBlueCells) {
-          cell.classList.add('blue');
-          blueCells++;
-        }
-      }
-    });
-    cell.addEventListener('mouseenter', () => {
-      if (isMouseDown) {
-        if (!cell.classList.contains('blue') && !cell.classList.contains('red')) {
-          cell.classList.add('red');
-        } else if (cell.classList.contains('red')) {
-          cell.classList.remove('red');
-        }
-      }
-    });
-    cell.addEventListener('mouseup', () => {
-      isMouseDown = false;
-    });
-    cell.addEventListener('contextmenu', (event) => {
-      event.preventDefault();
-      if (!cell.classList.contains('blue')) {
-        if (cell.classList.contains('red')) {
-          cell.classList.remove('red');
-        } else {
-          cell.classList.add('red');
-        }
-      }
-    });
     container.appendChild(cell);
   }
 
-  // Set container width and height based on grid size
-  container.style.width = gridWidth * 50 + "px";
-  container.style.height = gridHeight * 50 + "px";
+  // Functions for mouse event handling
+  function handleMouseDown() {
+    isMouseDown = true;
+    const cell = this;
+    if (!cell.classList.contains("red")) {
+      if (cell.classList.contains("blue")) {
+        cell.classList.remove("blue");
+        blueCells--;
+      } else if (blueCells < maxBlueCells) {
+        cell.classList.add("blue");
+        blueCells++;
+      }
+    }
+  }
 
-  // Initialize variables for mouse tracking and cell coloring
-  var isMouseDown = false;
-  var maxBlueCells = 2;
-  var blueCells = 0;
+  function handleMouseEnter() {
+    if (isMouseDown) {
+      const cell = this;
+      if (!cell.classList.contains("blue") && !cell.classList.contains("red")) {
+        cell.classList.add("red");
+      } else if (cell.classList.contains("red")) {
+        cell.classList.remove("red");
+      }
+    }
+  }
+
+  function handleMouseUp() {
+    isMouseDown = false;
+  }
+
+  function handleContextMenu(event) {
+    event.preventDefault();
+    const cell = this;
+    if (!cell.classList.contains("blue")) {
+      if (cell.classList.contains("red")) {
+        cell.classList.remove("red");
+      } else {
+        cell.classList.add("red");
+      }
+    }
+  }
 }
