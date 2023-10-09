4160800ae1f266b7a064efcea35b4ff97914108b
dusanlecic <dusan.lecic5@gmail.com>
2023-10-09 11:37:15 +0200

    modify index setup

--- src/index.py
+++ src/index.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+from flask import request, jsonify
+from astar import astar
 
 app = Flask(__name__, template_folder = '../templates/', static_folder='../templates/static/')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/', methods=["POST"])
+def handle_grid_data():
+    try:
+        data = request.get_json()
+        print("WORK WITH DATA")
+        respone_data = {"message" : "Grid data received and processed"}
+        return jsonify(respone_data), 200   
+    except:
+        return jsonify({"error" : str("error")}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
