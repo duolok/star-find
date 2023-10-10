0d4632621979eeabe2e92321d96cf0e39bd0d204
dusanlecic <dusan.lecic5@gmail.com>
2023-10-10 10:54:40 +0200

    astar for custom matrix size and obstacles

--- src/astar.py
+++ src/astar.py
@@ -1,20 +1,22 @@
 import math
 import heapq
 
-def initialize_costs(size, start):
-    costs = [[math.inf] * size[1] for i in range(size[0])]
-    (x, y) = start
-    costs[x-1][y-1] = 0
-    return costs
+# test case to be deleated one the parser is done
+size = (100, 100)
+start = (5, 3)
+end = (6, 9)
 
-def distance_heuristic(node, goal):
-    (x, y) = node
-    (u, v) = goal
-    return math.sqrt(abs(x - u) ** 2 + abs(y - v) ** 2)
+obstacles = {
+    (3, 4), (3, 5), (3, 6), (3, 7), (3, 8),
+    (4, 5),
+    (5, 5), (5, 7), (5, 9),
+    (6, 2), (6, 3), (6, 4), (6, 5), (6, 7),
+    (7, 7)
+}
 
-def successors(state, visited_nodes, obstacles, size):
+def successors(state, visited_nodes, matrix, obstaclestwo):
     (row, col) = state
-    (max_row, max_col) = size
+    (max_row, max_col) = matrix
     succ_states = []
     if row > 1:
         succ_states += [(row-1, col)]
@@ -24,42 +26,59 @@ def successors(state, visited_nodes, obstacles, size):
         succ_states += [(row+1, col)]
     if col < max_col:
         succ_states += [(row, col+1)]
-    return [s for s in succ_states if s not in visited_nodes if s not in obstacles]
 
-def astar(coordinates, size, obstacles):
-    start, end = coordinates[0], coordinates[1]
+    return [s for s in succ_states if s not in visited_nodes if s not in obstaclestwo]
+
+def initialize_costs(matrix, start):
+    (h, w) = matrix
+    costs = [[math.inf] * w for i in range(h)]
+    (x, y) = start
+    costs[x-1][y-1] = 0
+    return costs
+
+def heuristic(node, goal):
+    (x, y) = node
+    (u, v) = goal
+    return math.sqrt(abs(x-u) ** 2 + abs(y - v) ** 2)
+
+def astar_with_path(start, end, matrix, obstacles):
     frontier = []
     internal = set()
     heapq.heappush(frontier, (0, start))
-    costs = initialize_costs(size, start)
+    costs = initialize_costs(matrix, start)
 
     def get_distance_from_start(node):
-        return costs[node[0] -  1][node[1] - 1]
-
+        return costs[node[0]-1][node[1]-1]
+    
     def set_distance_from_start(node, new_distance):
-        costs[node[0] - 1][node[1] - 1] = new_distance
+        costs[node[0]-1][node[1]-1] = new_distance
 
     def get_shortest_path(end_node):
         path = [end_node]
         distance = get_distance_from_start(end_node)
         while distance > 0:
-            for neighbor in successors(node, internal, size, obstacles):
-                new_distance = get_distance_from_start(node) + 1
+            for neighbor in successors(path[-1], [], matrix, obstacles):
+                new_distance = get_distance_from_start(neighbor)
                 if new_distance < distance:
                     path += [neighbor]
                     distance = new_distance
                     break
         return path
 
+    steps = 0
     while len(frontier) > 0:
+        steps += 1
         (priority, node) = heapq.heappop(frontier)
         if node == end:
-            return get_shortest_path(end)
-        internal.add(node) 
-        successor_nodes = successors(node, internal, obstacles, size)
+            return get_shortest_path(end) 
+        internal.add(node)
+        successor_nodes = successors(node, internal, matrix, obstacles)
         for s in successor_nodes:
             new_distance = get_distance_from_start(node) + 1
             if new_distance < get_distance_from_start(s):
                 set_distance_from_start(s, new_distance)
                 frontier = [n for n in frontier if s != n[1]]
-                heapq.heappush(frontier, (new_distance + distance_heuristic(s, end), s))
+                heapq.heappush(frontier, (new_distance + heuristic(s, end), s))
+
+if __name__ == "__main__":
+    print(astar_with_path(start, end, size, obstacles))
