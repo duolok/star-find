e988e69d167a8f673b9704a4083ecd8cbcedd371
dusanlecic <dusan.lecic5@gmail.com>
2023-11-20 03:14:11 +0100

    refactor: small tweaks

--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask import request, jsonify
 from astar import astar
+from parser import parse_grid
 
 app = Flask(__name__, template_folder = '../templates/', static_folder='../templates/static/')
 
@@ -12,11 +13,11 @@ def home():
 def handle_grid_data():
     try:
         data = request.get_json()
-        print("WORK WITH DATA")
-        respone_data = {"message" : "Grid data received and processed"}
-        return jsonify(respone_data), 200   
-    except:
-        return jsonify({"error" : str("error")}), 400
+        start, end, matrix, obstacles = parse_grid(data)
+        path = astar(start, end, matrix, obstacles)
+        return jsonify({"path": path}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
