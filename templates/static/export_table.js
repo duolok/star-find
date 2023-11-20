634304fdff4fceaf9eedeedeab0103a88784ad43
dusanlecic <dusan.lecic5@gmail.com>
2023-11-20 03:12:39 +0100

    add: a* animations*

--- templates/static/export_table.js
+++ templates/static/export_table.js
@@ -20,9 +20,47 @@ function exportGrid() {
     })
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+        animateSearch(data.path);
     })
     .catch((error) => {
         console.error("Error: ", error);
     });
 }
+function animateSearch(path) {
+    const cells = document.querySelectorAll(".cell");
+    const delay = 20; // milliseconds
+
+    function colorCell(row, col, color) {
+        const cell = document.querySelector(`.cell[data-row='${row}'][data-col='${col}']`);
+        cell.classList.add(color);
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function animate() {
+        for (const node of path) {
+            const [row, col] = node;
+
+            // Skip animating blue squares
+            const cell = document.querySelector(`.cell[data-row='${row}'][data-col='${col}']`);
+            if (cell.classList.contains("blue")) {
+                continue;
+            }
+
+            // Highlight the current node being checked
+            colorCell(row, col, "checking");
+            await sleep(delay);
+
+            // Remove the highlight and color the cell in the path
+            cell.classList.remove("checking");
+            colorCell(row, col, "path");
+
+            await sleep(delay);
+        }
+    }
+
+    animate();
+}
+
